429f2b076e7706ea4b2577e8a7debbe70349939a
“MohanReddyASPL” <130907621+ArunChowdary2132@users.noreply.github.com>
2023-09-20 11:57:17 +0530

    var2

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb+srv://AnikaSterilis:Anika123@cluster0.foyqb0v.mongodb.net/Sensordb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+});
+
+const sensorSchema = {
+  sensorValue: String,
+}
+const sensor = mongoose.model('sensor', sensorSchema);
 app.get('/data', (req, res) => {
-  const sensorValue = req.query.value;
-  console.log(`Received sensor data: ${sensorValue}`);
+  let newData = ({
+    sensorValue: req.query.value,
+  // console.log(`Received sensor data: ${sensorValue}`);
+  })  ;
+  newData.sensorValue();
   // Process the sensor data as needed
 
   res.sendStatus(200); // Send a response back to the Arduino
