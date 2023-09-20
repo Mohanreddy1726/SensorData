369eca77f76dd8d407b10ad4c879c849b2301bb6
“MohanReddyASPL” <130907621+ArunChowdary2132@users.noreply.github.com>
2023-09-20 10:43:38 +0530

    ArduinoR4commit

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
+
 const app = express();
-const port = 3000; // Adjust the port as needed
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+const port = 3000; // Change this to your desired port
 
-mongoose.connect("mongodb+srv://mohananika123:Anika123@cluster0.kqje6p6.mongodb.net/Employeedb");
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connected to MongoDB');
-});
-const SensorDataSchema = {
-    httpRequestData: String
-}
-const sensordata = mongoose.model('sensordata', SensorDataSchema);
-// Endpoint to handle incoming data
-app.post('/data', (req, res) => {
-    // Retrieve data from the POST request
-    const { httpRequestData } = req.body;
-    let newData = new sensordata ({
-        httpRequestData
-    });
-newData.save();
-    // Store or process the data as needed
-    // For example, you can log it to the console
-    console.log(`Sensor Name: ${sensorName}, Data: ${data}`);
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
 
-    // Respond with a success message or any other required response
-    res.send('<script>alert("Data sent for verification!"); window.location.href = "/";</script>');
+// Handle POST requests with sensor data
+app.post('/sensor-data', (req, res) => {
+  const sensorData = req.body;
+  // Process sensor data here
+  console.log('Received sensor data:', sensorData);
+  res.sendStatus(200);
 });
 
-app.get('/', function (req, res) {
-    res.render('index')
-});
-app.get('/data', function (req, res) {
-    res.render('data')
-});
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
